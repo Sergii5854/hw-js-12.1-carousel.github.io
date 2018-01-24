b788f587bfea134f6385bb620af85698637b4550
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-01-24 22:31:10 +0200

    select all img

--- src/index.js
+++ src/index.js
@@ -38,11 +38,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     init(selector) {
       console.log("init", selector);
-      document.querySelector(selector)
+      let wrap =  document.querySelector(selector)
       let i;
       let slideIndex = 1
       let n =1;
-      let carousel = document.querySelector(selector);
+      let carousel = wrap.querySelectorAll('img');
       console.log(carousel.length);
       if (n > carousel.length) {
         slideIndex = 1
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (n < 1) {
         slideIndex = carousel.length
       }
-      for (i = 0; i < x.length; i++) {
+      for (i = 0; i < carousel.length; i++) {
         carousel[i].style.display = "none";
       }
       carousel[slideIndex - 1].style.display = "block";
