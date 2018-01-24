d3738c6ae5ec8261270974d5fe844ef1d974f57b
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-01-24 21:34:12 +0200

    create own class carousel

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,39 +2,45 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
-    },
-    resolve: {
-        extensions: [
-            '.ts',
-            '.js'
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            template: './src/index.html'
-        })
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js'
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                include: path.resolve()
-            },
-            {
-                test: /\.styl/,
-                loader: 'style-loader!css-loader!stylus-loader'
-            },
-            {
-                test: /\.json/,
-                loader: 'json-loader'
-            }
-        ]
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: path.resolve()
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      },
+
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+      {
+        test: /\.json/,
+        loader: 'json-loader'
+      }
+
+    ]
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import './style.styl';
 
-window.onload =  () => {
+document.addEventListener("DOMContentLoaded",function() {
+  // var slideIndex = 1;
+  // showDivs(slideIndex);
+  // var n = 1;
+  // document.querySelector('.right').addEventListener('click', function () {
+  //   console.log("right")
+  //   showDivs(slideIndex += n);
+  // });
+  // document.querySelector('.left').addEventListener('click', function () {
+  //   console.log("left")
+  //   showDivs(slideIndex += n);
+  // });
+  // function showDivs(n) {
+  //   var i;
+  //   var x = document.getElementsByClassName("mySlides");
+  //   if (n > x.length) {
+  //     slideIndex = 1
+  //   }
+  //   if (n < 1) {
+  //     slideIndex = x.length
+  //   }
+  //   for (i = 0; i < x.length; i++) {
+  //     x[i].style.display = "none";
+  //   }
+  //   x[slideIndex - 1].style.display = "block";
+  // }
 
 
-};
+  class BSCarousel {
+
+    constructor(config) {
+
+    }
+  }
+
+  // new BSCarousel({
+  //   selector: "#siema1"
+  // });
+  // new BSCarousel({
+  //   selector: "#siema"
+  // });
+
+
+});
