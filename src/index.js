12ce4eaf57bff312d6fc06fcf8536f85a7bd33f9
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-01-24 22:21:43 +0200

    rewrite by my own function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.styl';
 
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function () {
   // var slideIndex = 1;
   // showDivs(slideIndex);
   // var n = 1;
@@ -31,86 +31,36 @@ document.addEventListener("DOMContentLoaded",function() {
   class BSCarousel {
 
     constructor(options) {
-      this.config = BSCarousel.mergeSettings(options);
-      console.log(this.config);
-      this.selector = typeof this.config.selector === 'string'
-          ? document.querySelector(this.config.selector)
-          : this.config.selector;
-      if (this.selector === null) {
-        throw new Error('Something wrong with your selector ');
-      }
-      this.resolveSlidesNumber();
 
-
-      this.selectorWidth = this.selector.offsetWidth;
-      this.innerElements = [].slice.call(this.selector.children);
-      this.currentSlide = Math.max(0, Math.min(this.config.startIndex, this.innerElements.length - this.perPage));
-      this.transformProperty = BSCarousel.webkitOrNot();
-
-      // Bind all event handlers for referencability
-      ['resizeHandler',
-        'touchstartHandler',
-        'touchendHandler',
-        'touchmoveHandler',
-        'mousedownHandler',
-        'mouseupHandler',
-        'mouseleaveHandler',
-        'mousemoveHandler',
-        'clickHandler'].forEach(method => {
-        this[method] = this[method].bind(this);
-      });
-
-      this.init();
+      this.selector = options;
+      this.init(this.selector);
     }
 
-    static mergeSettings(options) {
-      const settings = {
-        selector: '.bs-carousel',
-        duration: 200,
-        easing: 'ease-out',
-
-        onInit: () => {},
-        onChange: () => {},
-      };
-
-      const userSttings = options;
-      for (const attrname in userSttings) {
-        settings[attrname] = userSttings[attrname];
+    init(selector) {
+      console.log("init", selector);
+      document.querySelector(selector)
+      let i;
+      let slideIndex = 1
+      let n =1;
+      let carousel = document.querySelector(selector);
+      console.log(carousel.length);
+      if (n > carousel.length) {
+        slideIndex = 1
       }
-
-      return settings;
-    }
-    resolveSlidesNumber() {
-      if (typeof this.config.perPage === 'number') {
-        this.perPage = this.config.perPage;
+      if (n < 1) {
+        slideIndex = carousel.length
       }
-      else if (typeof this.config.perPage === 'object') {
-        this.perPage = 1;
-        for (const viewport in this.config.perPage) {
-          if (window.innerWidth >= viewport) {
-            this.perPage = this.config.perPage[viewport];
-          }
-        }
+      for (i = 0; i < x.length; i++) {
+        carousel[i].style.display = "none";
       }
-    }
-
-    static webkitOrNot() {
-      const style = document.documentElement.style;
-      if (typeof style.transform === 'string') {
-        return 'transform';
-      }
-      return 'WebkitTransform';
+      carousel[slideIndex - 1].style.display = "block";
     }
 
 
   }
 
-  new BSCarousel({
-    selector: ".bs-carousel"
-  });
-  new BSCarousel({
-    selector: ".bs-carousel3333"
-  });
+  new BSCarousel(".bs-carousel");
+  new BSCarousel(".bs-carousel3333");
 
 
 });
