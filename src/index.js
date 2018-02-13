48bef61c9f4669c806e331bf9c022c92c6e7c890
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-02-13 13:11:20 +0200

    add coments

--- src/index.js
+++ src/index.js
@@ -2,50 +2,51 @@ import './style.styl';
 
 
 document.addEventListener("DOMContentLoaded", function () {
-
+// create instance of carousel
   class BScarousel {
-
+    // constructor with one argument - string of selector
     constructor(options) {
 
       this.selector = options;
-
+      // handling err if something wrong argument, and don't destroy all JS
       if (document.querySelector(this.selector) === null) {
         throw new Error('Something wrong with your selector 😭');
       }
-
+      // variables
       this.carousel = document.querySelector(this.selector);
       this.carouselImg = this.carousel.querySelectorAll('img');
       this.carouselLength = this.carouselImg.length;
       this.build()
 
     }
-
+    // method for current slider/ index / img
     currentSlide(n) {
       this.showSlides(this.index = n);
     }
-
+    // method for change slider
     plusSlides(n) {
       this.showSlides(this.index += n);
     }
-
+    // method for create dots
     createIndicators() {
       let wrapIndicators = document.createElement('ul');
       wrapIndicators.classList.add('wrap-indicators')
 
+      if (this.carouselLength > 1) {
+        this.carousel.appendChild(wrapIndicators);
+        for (let i = 1; i < this.carouselLength + 1; i++) {
 
-      this.carousel.appendChild(wrapIndicators);
-      for (let i = 1; i < this.carouselLength + 1; i++) {
+          let dot = document.createElement('li');
+          dot.classList.add('dot');
+          dot.setAttribute('value', i);
 
-        let dot = document.createElement('li');
-        dot.classList.add('dot');
-        dot.setAttribute('value', i);
+          dot.addEventListener('click', (e) => {
+            this.index = e.target.value;
+            this.showSlides(this.index)
+          });
 
-        dot.addEventListener('click', (e) => {
-          this.index = e.target.value;
-          this.showSlides(this.index)
-        });
-
-        wrapIndicators.appendChild(dot);
+          wrapIndicators.appendChild(dot);
+        }
       }
     }
 //How TO - Slideshow https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
@@ -75,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-
+    // control swipe direction ( change slider for + or -  direction)
     controls(direction) {
       if (direction === 'left') {
         this.plusSlides(1);
@@ -83,12 +84,12 @@ document.addEventListener("DOMContentLoaded", function () {
         this.plusSlides(-1);
       }
     }
-
+    // start point off swipe, get direction of swipe
     swipeStart(e) {
       this.swiping = true;
       this.startX = e.pageX;
     }
-
+// direction of swipe from start point
     swipeMove(event) {
 
       let checkSwipe = event.touches
@@ -103,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       event.preventDefault()
     }
-
+// ending point, get direction
     swipeEnd(event) {
       this.swiping = false;
       if (event.touches) {
@@ -119,8 +120,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     addEvents() {
       this.startX = undefined;
-
-
       this.carousel.ondragstart = () => false;
 
       this.carousel.onmousedown = this.swipeStart.bind(this);
@@ -133,6 +132,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 
+    //build all methods to create carousel
     build() {
 
       this.index = 1;
