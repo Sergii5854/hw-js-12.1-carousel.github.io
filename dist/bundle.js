72ef4ee95f82f18d6f0f6c00fe5f9d276bb274b8
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-02-19 10:51:56 +0200

    update build

--- dist/bundle.js
+++ dist/bundle.js
@@ -9030,50 +9030,41 @@ module.exports = function (regExp, replace) {
 __webpack_require__(329);
 
 document.addEventListener("DOMContentLoaded", function () {
+  //create instance of carousel
   class BScarousel {
     constructor(options) {
       this.selector = options;
       if (document.querySelector(this.selector) === null) {
+        // handling error
         throw new Error('Something wrong with your selector 😭');
       }
-      this.build();
+      this.build(); // build instance carousel
     }
 
-    carouselVaribls() {
-      this.swiping = false;
-      this.previous = null;
+    // declarate all variables we need to operate carousel
+    carouselVariabls() {
       this.carousel = document.querySelector(`${this.selector}`);
       this.carouselList = this.carousel.querySelector(`.carousel__display`);
-
       this.carouselImg = this.carousel.querySelectorAll('img');
-
       this.carouselLength = this.carouselImg.length;
-      this.itemWidth = parseFloat(getComputedStyle(this.carousel.querySelector('img')).width);
-      this.itemHeight = parseFloat(getComputedStyle(this.carousel.querySelector('img')).height);
       this.carouselWidth = (this.carouselLength - 1) * this.itemWidth;
-      this.carouselSwipe = Math.abs(parseFloat(getComputedStyle(this.carouselList).left));
-
-      this.item = this.itemWidth || this.carouselSwipe / this.itemWidth;
       this.wrapIndicators = document.createElement('ul');
 
-      console.log(" this.carouselList", this.carouselList);
-      console.log(" this.carouselLength", this.carouselLength);
-      console.log(" this.carouselWidth", this.carouselWidth);
-      console.log(" this.itemWidth", this.itemWidth);
-      console.log(" this.item", this.item);
-      console.log(" this.carouselSwipe", this.carouselSwipe);
+      this.swiping = false;
+      this.previous = null;
+      this.carouselSwipe = null;
+      this.item = 0;
     }
 
+    // create indicators - dots
     createIndicators() {
       this.wrapIndicators.classList.add('wrap-indicators');
       if (this.carouselLength > 1) {
         this.carousel.appendChild(this.wrapIndicators);
         for (let i = 0; i < this.carouselLength; i++) {
-
           let dot = document.createElement('li');
           dot.classList.add('dot');
           dot.setAttribute('value', i);
-
           dot.addEventListener('click', e => {
             this.index = e.target.value;
             this.carouselList.style.left = -this.index * this.itemWidth + 'px';
@@ -9083,20 +9074,22 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-    swipeStart() {
+    // start swipe/ dragging event
+    swipeStart(e) {
       this.swiping = true;
+      this.startX = e.pageX;
     }
 
+    //declarate  proses of dragging event
     swipeMove(event) {
       if (this.swiping) {
         if (this.previous) {
-          this.left = parseInt(this.carouselList.style.left || 0) + (event.clientX - this.previous) * 2;
+          this.left = parseInt(this.carouselList.style.left || 0) + (event.clientX - this.previous) * 2; //get the meaning of our movement
 
-          if (this.left > 0) {
+          if (this.left >= 0) {
             this.left = 0;
-          } else if (this.carouselWidth - this.itemWidth < Math.abs(this.left)) {
-            console.log(this.left, this.carouselWidth - this.itemWidth);
-            this.left = -this.carouselWidth;
+          } else if (this.carouselWidth < Math.abs(this.left)) {
+            this.left = 0;
           }
           this.carouselList.style.left = this.left + 'px';
         }
@@ -9104,27 +9097,41 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       event.preventDefault();
     }
-
+    //end  proses of dragging event;  expect the final position
     swipeEnd() {
-
-      if (this.carouselSwipe % this.itemWidth > this.itemWidth / this.carouselLength) {
-        this.item++;
-      }
+      this.carouselSwipe = Math.abs(parseFloat(getComputedStyle(this.carouselList).left)); // get value of swipe
+      this.item = this.carouselSwipe / this.itemWidth | 0 || 0; // get current item
 
       if (this.carouselSwipe >= this.carouselWidth) {
         this.item = 0;
+        this.carouselList.style.left = '0px';
+        this.swiping = false;
       }
 
       if (this.carouselSwipe <= 0) {
-        this.item = this.carouselLength;
+        this.item = this.carouselLength - 1;
+        this.carouselList.style.left = this.carouselWidth + 'px';
+        this.swiping = false;
       }
-      this.carouselList.style.left = -(this.item * this.itemWidth) - this.itemWidth + 'px';
-      this.swiping = false;
-      this.previous = null;
-    }
+      // swiping function
+      if (event.touches) {
+        event.pageX < this.startX ? this.controls('left') : this.controls('right');
+      } else {
+        event.pageX < this.startX ? this.controls('left') : this.controls('right');
+      }
 
+      this.carouselList.style.left = -Math.abs(this.item * this.itemWidth) + 'px'; // expect the final position
+      this.previous = null;
+      this.swiping = false;
+    }
+    // swipe event
+    controls(direction) {
+      if (direction === 'left') {
+        this.item++;
+      }
+    }
+    //  add event to current instance of carousel
     addEvents() {
-      console.log("start");
       this.carouselList.onmousedown = this.swipeStart.bind(this);
       this.carouselList.onmousemove = this.swipeMove.bind(this);
       this.carouselList.onmouseup = this.swipeEnd.bind(this);
@@ -9133,24 +9140,15 @@ document.addEventListener("DOMContentLoaded", function () {
       this.carouselList.ontouchmove = this.swipeMove.bind(this);
       this.carouselList.ontouchend = this.swipeEnd.bind(this);
     }
-    removeEvents() {
-      console.log("remove");
-      this.carouselList.onmousedown = null;
-      this.carouselList.onmousemove = null;
-      this.carouselList.onmouseup = null;
 
-      this.carouselList.ontouchstart = null;
-      this.carouselList.ontouchmove = null;
-      this.carouselList.ontouchend = null;
-    }
-
+    // build our instance
     build() {
-      this.carouselVaribls();
+      this.carouselVariabls();
       this.createIndicators();
       this.addEvents();
     }
   }
-
+  // create instances of carousel
   new BScarousel(".bs-carousel");
   new BScarousel(".bs-carousel-test");
   new BScarousel(".bs-carousel2");
